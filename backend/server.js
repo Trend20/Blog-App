1341b4f939c70950899c71789862ce0919a0e88a
DevEnock <enockomondi305@gmail.com>
2022-03-21 17:17:25 +0300

    seeting up the backend server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 5500;
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+require('dotenv').config();
+
+app.listen(PORT, () =>{
+  console.log(`Application is listening on port ${PORT}`);
+});
