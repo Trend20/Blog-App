499434ffb5c43aaef91a18e2ea4d71e75478e6ed
DevEnock <enockomondi305@gmail.com>
2022-03-21 18:41:13 +0300

    seeting up the backend server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+// routes
+const userRoutes = require('./routes/User');
+const articlesRoutes = require('./routes/User');
+
 const app = express();
 
 const PORT = process.env.PORT || 5500;
@@ -10,6 +14,15 @@ const PORT = process.env.PORT || 5500;
 // middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+const url = process.env.DATABASE_URL;
+
+// connect to the db
+const connection = mongoose.connection;
+mongoose.connect(url)
+// using project routes
+app.use('user', userRoutes);
+app.use('articles', articlesRoutes);
 
 require('dotenv').config();
 
