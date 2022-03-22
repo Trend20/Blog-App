58fee6d656e3367fa98a25a3979cc36c232b5278
DevEnock <enockomondi305@gmail.com>
2022-03-22 11:17:56 +0300

    adding validations

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+
+const User = require('../models/Users.model');
+
+
+router.route('/register').post((req, res) =>{
+  
+})
 
 
 module.exports = router;
