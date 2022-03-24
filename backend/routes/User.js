42a69854d4c00696376ee94a6e8185fd12fce79f
DevEnock <enockomondi305@gmail.com>
2022-03-24 09:56:00 +0300

    adding register route

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -5,10 +5,52 @@ const router = express.Router();
 
 const User = require('../models/Users.model');
 
+// import input validations
+// const validateLoginInput = require('../middleware/auth');
+const {validateRegisterInput, validateLoginInput} = require('../middleware/auth');
 
+
+
+// REGISTER ROUTE
 router.route('/register').post((req, res) =>{
-  
-})
+
+  const {error, isValid} = validateRegisterInput(req.body);
+
+  if(!isValid){
+    return res.status(400).json(error);
+  }
+
+  User.findOne({email:req.body.email})
+      .then(user =>{
+        if(user){
+          return res.status(400).json({email:"Email already exists"});
+        }else{
+          const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+          });
+
+          // hashing the password
+          const limit = 8;
+          bcrypt.genSalt(limit, (err, salt) =>{
+            bcrypt.hash(newUser.password, salt, (err, hash) =>{
+              if(err) throw err;
+              newUser.password = hash;
+              newUser.save()
+                     .then(user => res.json(user))
+                     .catch(err => console.log(err))
+            });
+          });
+        }
+      });
+});
+
+
+
+// LOGIN ROUTE
+
+router.route('/login').post()
 
 
 module.exports = router;
