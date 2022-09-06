c4a8763bd35baa8f23ef794fac7cc7bbc89d8bf1
DevEnock <enockomondi305@gmail.com>
2022-09-06 17:25:05 +0300

    Header fix (#6)
    * adding the sidebar
    
    * adding pages components
    
    * fixing application routes
    
    * fixing application routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import TopBar from './components/TopBar/TopBar';
 import Home from './pages/Home/Home';
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Settings from "./pages/Settings/Settings";
+import Single from "./pages/Single/Single";
+import Write from "./pages/Write/Write";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
+  const currentUser = true;
   return (
-    <>
+    <BrowserRouter>
       <TopBar />
-      <Home />
-    </>
+      <Routes>
+        <Route exact path="/"  component={<Home />} />
+        <Route path="/posts"  component={<Home />} />
+        <Route path="/register"  component={currentUser ? <Home /> : <Register />} /> 
+        <Route path="/login"  component={currentUser ? <Home /> : <Login />} />
+        <Route path="/post/:id"  component={<Single />} />
+        <Route path="/write" component={currentUser ? <Write /> : <Login />} />
+        <Route path="/settings" component={currentUser ? <Settings /> : <Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
+import { useLocation } from "react-router";
 import Header from '../../components/Header/Header';
+import Posts from '../../components/Posts/Posts';
+import Sidebar from '../../components/Sidebar/Sidebar';
 import './Home.css';
 
 const Home = () => {
+  const location = useLocation();
   return (
-    <div className='homepage'>
-      <Header />
-      Homepage
-    </div>
+    <>
+     <Header />
+      <div className='home'>
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
   )
 }
 
