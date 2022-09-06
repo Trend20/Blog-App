7f41d32b90dc451f6ce156a71cbf06bf1364648e
Trend20 <enockomondi305@gmail.com>
2022-09-06 17:21:50 +0300

    fixing application routes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import { useLocation } from "react-router";
 import Header from '../../components/Header/Header';
 import Posts from '../../components/Posts/Posts';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import './Home.css';
 
 const Home = () => {
+  const location = useLocation();
   return (
     <>
      <Header />
