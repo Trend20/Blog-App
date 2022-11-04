bcb146a29e97f90b30125cd520bce6c4e7ba69e8
Trend20 <enockomondi305@gmail.com>
2022-11-04 10:11:35 +0300

    navbar fixes

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import './TopBar.css';
 
 const TopBar = () => {
+  const user = true;
   return (
     <div className='topBar'>
       <div className="topLeft">
@@ -11,15 +13,45 @@ const TopBar = () => {
       </div>
       <div className="topCenter">
         <ul className="topBarList">
-          <li className="topBarListItem">HOME</li>
+          <li className="topBarListItem">
+            <Link className='link' to="/">
+              HOME
+            </Link>
+          </li>
           <li className="topBarListItem">ABOUT</li>
           <li className="topBarListItem">WORK</li>
           <li className="topBarListItem">CONTACT</li>
-          <li className="topBarListItem">LOGOUT</li>
+          <li className="topBarListItem">
+            <Link className='link' to="/write">
+              WRITE
+            </Link>
+          </li>
+          {user && <li className="topListItem">LOGOUT</li>}
         </ul>
       </div>
       <div className="topRight">
-        <img src="/assets/profile.jpg" alt="profile" className="topImg" />
+      {user ? (
+          <Link className="link" to="/settings">
+            <img
+              className="topImg"
+              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
         <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
     </div>
