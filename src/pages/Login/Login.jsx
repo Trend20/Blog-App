647637770c3623bdfc591851c2e0fb55d218992a
Trend20 <enockomondi305@gmail.com>
2022-11-15 17:37:48 +0300

    styling the login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,53 +2,63 @@ import axios from "axios";
 import { useContext, useRef } from "react";
 import { Context } from "../../context/Context";
 import { Link } from "react-router-dom";
+import { FaSignInAlt } from "react-icons/fa";
 import "./Login.css";
 
 const Login = () => {
-  
   const userRef = useRef();
   const passwordRef = useRef();
   const { user, dispatch, isFetching } = useContext(Context);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch({type: "LOGIN_START"});
+    dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post('http://localhost:5500/api/auth/login',{
+      const res = await axios.post("http://localhost:5500/api/auth/login", {
         username: userRef.current.value,
-        password: passwordRef.current.value
-      })
-      dispatch({type: "LOGIN_SUCCESS", payload: res.data});
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     } catch (error) {
-      dispatch({type: "LOGIN_FAILURE"});
+      dispatch({ type: "LOGIN_FAILURE" });
     }
   };
-console.log(user);
+  // console.log(user);
   return (
     <div className="login">
-      <span className="loginTitle">Login</span>
-      <form className="loginForm" onSubmit={handleSubmit}>
-        <label>Username</label>
-        <input
-          className="loginInput"
-          type="text"
-          placeholder="Enter your username..."
-          ref={userRef}
-        />
-        <label>Password</label>
-        <input
-          className="loginInput"
-          type="password"
-          placeholder="Enter your password..."
-          ref={passwordRef}
-        />
-        <button className="loginButton" type="submit" disabled={isFetching}>
-          Login
+      <div className="login-contents">
+        <div className="login-header">
+          <i>
+            <FaSignInAlt />
+          </i>
+          <h1>Welcome!</h1>
+          <h5>Sign in to your account</h5>
+        </div>
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <label>Username</label>
+          <input
+            className="loginInput"
+            type="text"
+            placeholder="Enter your username..."
+            ref={userRef}
+          />
+          <label>Password</label>
+          <input
+            className="loginInput"
+            type="password"
+            placeholder="Enter your password..."
+            ref={passwordRef}
+          />
+          <button className="loginButton" type="submit" disabled={isFetching}>
+            Login
+          </button>
+        </form>
+        <button className="loginRegisterButton">
+          <Link to="/register" className="link">
+            Register
+          </Link>
         </button>
-      </form>
-      <button className="loginRegisterButton">
-        <Link to="/register" className="link">Register</Link>
-      </button>
+      </div>
     </div>
   );
 };
