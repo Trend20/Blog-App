98071d3d1418adce3cf7b5c8680195de51cfb5d1
Trend20 <enockomondi305@gmail.com>
2022-11-16 00:22:30 +0300

    resgister page styles

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { CgProfile } from 'react-icons/cg';
+import { FiArrowRight } from 'react-icons/fi'
+import { MdFacebook } from 'react-icons/md';
+import { AiFillTwitterCircle, AiFillGithub } from 'react-icons/ai'
 import './Register.css';
 
 const Register = () => {
@@ -27,37 +31,53 @@ const Register = () => {
 
   return (
     <div className="register">
-      <span className="registerTitle">Register</span>
+     <div className="register-contents">
+     <div className="login-header">
+          <i>
+            <CgProfile />
+          </i>
+          <h1>Create Account!</h1>
+        </div>
       <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
         <input
           className="registerInput"
           type="text"
-          placeholder="Enter your username..."
           onChange={(e) => setUsername(e.target.value)}
         />
         <label>Email</label>
         <input
           className="registerInput"
           type="text"
-          placeholder="Enter your email..."
           onChange={(e) => setEmail(e.target.value)}
         />
         <label>Password</label>
         <input
           className="registerInput"
           type="password"
-          placeholder="Enter your password..."
           onChange={(e) => setPassword(e.target.value)}
         />
         <button className="registerButton" type="submit">
           Register
+          <i><FiArrowRight size= {25}/></i>
         </button>
+        <hr />
       </form>
-      <button className="registerLoginButton">
-        <Link to="/login" className="link">Login</Link>
-      </button>
+      <div className="social-icons">
+        <h5>Or create account using social media!</h5>
+        <span>
+          <i><MdFacebook size={25}/></i>
+          <i><AiFillTwitterCircle size={25}/></i>
+          <i><AiFillGithub size={25}/></i>
+        </span>
+      </div>
+      <p className="sign-up">Already a member? 
+        <Link to="/login" className="signup-link">
+            Login
+          </Link>
+        </p>
       {error && <span>Something went wrong!</span>}
+     </div>
     </div>
   );
 }
