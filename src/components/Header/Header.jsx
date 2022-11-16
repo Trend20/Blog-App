53e3f0b19881193744d522106ca6ac100d4c5b72
Trend20 <enockomondi305@gmail.com>
2022-11-16 22:27:00 +0300

    styling header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,7 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='header'>
-      <div className="headerTitles">
-        <span className='titleSm'>React & Node</span>
-        <span className='titleLg'>Blog</span>
-      </div>
-      <img className='headerImg' src="/assets/header.jpg" alt="header-img" />
+      <img className='headerImg' src="/assets/home2.jpg" alt="header-img" />
     </div>
   )
 }
