74879f5649236d2fae43bdb35ecaf55c94248705
Trend20 <enockomondi305@gmail.com>
2022-11-21 16:04:04 +0300

    adding email subscription state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,10 +8,6 @@ const Header = () => {
       <div className="headerTitles">
         <h3>Don't miss any update on our new articles!</h3>
         <p>Get our free articles direct on your email as soon as they get published!</p>
-        <form>
-          <input className='input' type="text" placeholder='Email Address' />
-          <button className='sub-btn' type='submit'>Subscribe</button>
-        </form>
         <Link className="scroll-down" to="posts" smooth={true} >
           <CgScrollV size={50} color="blue" />
         </Link>
--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import './Subscribe.css';
+import { useState } from "react";
+import "./Subscribe.css";
 
 const Subscribe = () => {
+  const [email, setEmail] = useState("");
+
+  // handle email change
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  // form submission
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>Subscribe</div>
-  )
-}
+    <form onSubmit={handleFormSubmit}>
+      <input
+        className="input"
+        value={email}
+        name="email_address"
+        onChange={handleEmailChange}
+        type="email"
+        placeholder="Email Address"
+      />
+      <button className="sub-btn" type="submit">
+        Subscribe
+      </button>
+    </form>
+  );
+};
 
 export default Subscribe;
