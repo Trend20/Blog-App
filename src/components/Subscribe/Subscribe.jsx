c5ca813e84ecc944d704a8cf2c01a38a7473db1c
Trend20 <enockomondi305@gmail.com>
2022-11-22 17:05:56 +0300

    subscribe func

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./Subscribe.css";
 
 const Subscribe = () => {
+  const [status, setStatus] = useState(null);
   const [email, setEmail] = useState("");
 
   // handle email change
@@ -9,7 +10,7 @@ const Subscribe = () => {
     setEmail(event.target.value);
   };
 
-  const FORM_URL = 'https://app.convertkit.com/forms/3801514/subscriptions';
+  const FORM_URL = "https://app.convertkit.com/forms/3801514/subscriptions";
   // form submission
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -18,37 +19,63 @@ const Subscribe = () => {
 
     try {
       const response = await fetch(FORM_URL, {
-        method: 'post',
+        method: "post",
         body: data,
-        headers:{
-          accept: 'application/json'
+        headers: {
+          accept: "application/json",
         },
-      })
+      });
+      setEmail("");
       const json = await response.json();
       console.log(response);
-      if(json.status === "success"){
+      if (json.status === "success") {
         return;
       }
     } catch (error) {
       console.log(error);
     }
-    setEmail('');
   };
 
   return (
-    <form action="https://app.convertkit.com/forms/3801514/subscriptions" onSubmit={handleFormSubmit}>
-      <input
-        className="input"
-        value={email}
-        name="email_address"
-        onChange={handleEmailChange}
-        type="email"
-        placeholder="Email Address"
-      />
-      <button className="sub-btn" type="submit">
-        Subscribe
-      </button>
-    </form>
+    <div className="subscription-container">
+      {status === "SUCCESS" && (
+        <div>
+          <p>
+            Welcome aboard{email ? `, ${email}` : ""}{" "}
+            <span role="img" aria-label="Ship">
+              🚢
+            </span>
+          </p>
+          <p>Please check your inbox to confirm the subscription!</p>
+        </div>
+      )}
+      {status === "ERROR" && (
+        <div>
+          <p>Oops, something went wrong...</p>
+          <p>
+            Please, <button onClick={() => setStatus(null)}>try again.</button>
+          </p>
+        </div>
+      )}
+      {status === null && (
+        <form
+          action="https://app.convertkit.com/forms/3801514/subscriptions"
+          onSubmit={handleFormSubmit}
+        >
+          <input
+            className="input"
+            value={email}
+            name="email_address"
+            onChange={handleEmailChange}
+            type="email"
+            placeholder="Email Address"
+          />
+          <button className="sub-btn" type="submit">
+            Subscribe
+          </button>
+        </form>
+      )}
+    </div>
   );
 };
 
