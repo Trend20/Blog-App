c94e8b6b942e171b926676ae92f751f1eb5826e4
Trend20 <enockomondi305@gmail.com>
2022-12-14 11:57:50 +0300

    adding login error messages

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useContext, useRef } from "react";
 import { Context } from "../../context/Context";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { FaSignInAlt } from "react-icons/fa";
 import { FiArrowRight } from 'react-icons/fi'
@@ -10,8 +11,9 @@ const Login = () => {
   const userRef = useRef();
   const passwordRef = useRef();
   const { user, dispatch, isFetching } = useContext(Context);
+  const { register, handleSubmit, watch, formState:{ errors }} = useForm();
 
-  const handleSubmit = async (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
@@ -35,19 +37,23 @@ const Login = () => {
           <h1>Welcome!</h1>
           <h5>Sign in to your account</h5>
         </div>
-        <form className="loginForm" onSubmit={handleSubmit}>
+        <form className="loginForm" onSubmit={handleSubmit(handleFormSubmit)}> 
           <label>Username</label>
           <input
             className="loginInput"
             type="text"
             ref={userRef}
+            {...register("username", { required: true })}
           />
+          <p className="error-text">{errors.username && <span>Enter username</span>}</p>
           <label>Password</label>
           <input
             className="loginInput"
             type="password"
             ref={passwordRef}
+            {...register("password", { required: true })}
           />
+          <p className="error-text">{errors.password && <span>Password is required</span>}</p>
           <button className="loginButton" type="submit" disabled={isFetching}>
             Login
             <i><FiArrowRight size= {25}/></i>
