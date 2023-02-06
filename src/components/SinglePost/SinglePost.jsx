52ec5c1b3389639d8e09287093d507e6404bbc0f
Trend20 <enockomondi305@gmail.com>
2023-02-06 17:03:08 +0300

    prettier format

--- src/components/SinglePost/SinglePost.jsx
+++ src/components/SinglePost/SinglePost.jsx
@@ -1,70 +1,87 @@
 import axios from "axios";
 import { useEffect, useContext, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import './SinglePost.css';
-import { Context } from '../../context/Context';
+import "./SinglePost.css";
+import { Context } from "../../context/Context";
 
 const SinglePost = () => {
   const location = useLocation();
-  const path = location.pathname.split('/')[2]
+  const path = location.pathname.split("/")[2];
   // console.log(path);
   const PF = "http://localhost:5500/images/";
   const { user } = useContext(Context);
-  const [title, setTitle ] = useState('');
-  const [description, setDescription] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const [updateMode, setUpdateMode] = useState(false);
   const [postItem, setPostItem] = useState({});
   // fetch the post using id
-  useEffect(() =>{
-    const getPost = async() =>{
-      const res = await axios.get('http://localhost:5500/api/posts/' + path)
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("http://localhost:5500/api/posts/" + path);
       setPostItem(res.data);
       setTitle(res.data.title);
       setDescription(res.data.description);
-    }
+    };
     getPost();
   }, [path]);
 
-  // delete 
-  const handleDelete = async() =>{
+  // delete
+  const handleDelete = async () => {
     try {
-      await axios.delete(`http://localhost:5500/api/posts/${postItem._id}`, {data: {username: user.username}});
+      await axios.delete(`http://localhost:5500/api/posts/${postItem._id}`, {
+        data: { username: user.username },
+      });
       window.location.replace("/");
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   // update
-  const handleUpdate = async () =>{
+  const handleUpdate = async () => {
     try {
-      await axios.put(`http://localhost:5500/api/posts/${postItem._id}`, {username: user.username, title, description});
+      await axios.put(`http://localhost:5500/api/posts/${postItem._id}`, {
+        username: user.username,
+        title,
+        description,
+      });
       // window.location.reload();
-      setUpdateMode(false)
+      setUpdateMode(false);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        { postItem.photo && (<img
-          className="singlePostImg"
-          src={PF + postItem.photo}
-          alt=""
-        />)}
-        {
-          updateMode ? <input type="text" value={title} className="singlePostTitleInput" onChange={(e) =>setTitle(e.target.value)}/> : (
-            <h1 className="singlePostTitle">
-         {title}
-         {postItem.username === user?.username && ( <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit" onClick={() => setUpdateMode(true)}></i>
-            <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
-          </div>)}
-        </h1>
-          )
-        }
+        {postItem.photo && (
+          <img className="singlePostImg" src={PF + postItem.photo} alt="" />
+        )}
+        {updateMode ? (
+          <input
+            type="text"
+            value={title}
+            className="singlePostTitleInput"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <h1 className="singlePostTitle">
+            {title}
+            {postItem.username === user?.username && (
+              <div className="singlePostEdit">
+                <i
+                  className="singlePostIcon far fa-edit"
+                  onClick={() => setUpdateMode(true)}
+                ></i>
+                <i
+                  className="singlePostIcon far fa-trash-alt"
+                  onClick={handleDelete}
+                ></i>
+              </div>
+            )}
+          </h1>
+        )}
         <div className="singlePostInfo">
           <span>
             Author:
@@ -76,14 +93,24 @@ const SinglePost = () => {
           </span>
           <span>{new Date(postItem.createdAt).toDateString()}</span>
         </div>
-       { updateMode ? <textarea value={description} className="singlePostDescInput" onChange={(e) =>setDescription(e.target.value)}></textarea> : ( <p className="singlePostDesc">
-          {description}
-        </p>)}
+        {updateMode ? (
+          <textarea
+            value={description}
+            className="singlePostDescInput"
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        ) : (
+          <p className="singlePostDesc">{description}</p>
+        )}
 
-        {updateMode && (<button className="singlePostUpdate" onClick={handleUpdate}>Update</button>)}
+        {updateMode && (
+          <button className="singlePostUpdate" onClick={handleUpdate}>
+            Update
+          </button>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SinglePost;
